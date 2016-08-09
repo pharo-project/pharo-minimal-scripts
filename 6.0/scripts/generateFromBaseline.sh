218b087b0d9a6a6ce4f5b405a638798af629aa31
Pavel Krivanek <pavel.krivanek@gmail.com>
2016-08-09 10:22:49 +0200

    full clone

--- 6.0/scripts/generateFromBaseline.sh
+++ 6.0/scripts/generateFromBaseline.sh
@@ -11,7 +11,11 @@ cd icon-packs
 wget http://github.com/pharo-project/pharo-icon-packs/archive/idea11.zip
 cd ..
 
-git clone --branch v$(cut -f1 ../version.txt) --depth 1 https://github.com/guillep/pharo-core.git
+#git clone --branch v$(cut -f1 ../version.txt) --depth 1 https://github.com/guillep/pharo-core.git
+git clone https://github.com/guillep/pharo-core.git
+cd pharo-core
+git checkout "tags/v$(cut -f1 ../version.txt)"
+cd ..
 
 cp -R ./pharo-minimal-scripts/6.0/baselines/* ./pharo-core/src/
 
